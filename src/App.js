41fd0fa9d1e2d2bd7e0024319801e86eb6bc429d
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-06 07:41:23 +0100

    feat: useEffect to send data to backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
+import Notification from './components/Notification';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
+  // console.log(isLoggedIn);
+  // const cartItems = useSelector((state) => state.cart.itemsList);
+  // console.log(cartItems);
+  useEffect(() => {
+    fetch(
+      'https://react-http-9648f-default-rtdb.firebaseio.com/cartItems.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <div className='App'>
+      <Notification type='Success' message={'This Message'} />
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
