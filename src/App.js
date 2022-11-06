4b807e222201f74b42e2d7fdad0fc860e589f218
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-11-06 08:10:56 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import './App.css';
 import Auth from './components/Auth';
 import Layout from './components/Layout';
 import Notification from './components/Notification';
+import { uiActions } from './store/ui-slice';
+
+let firstRender = true;
 
 function App() {
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
 
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  // console.log(isLoggedIn);
-  // const cartItems = useSelector((state) => state.cart.itemsList);
-  // console.log(cartItems);
   useEffect(() => {
-    fetch(
-      'https://react-http-9648f-default-rtdb.firebaseio.com/cartItems.json',
-      {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      }
+    if (firstRender) {
+      firstRender = false;
+      return;
+    }
+    //send state as sending request
+    dispatch(
+      uiActions.showNotification({
+        open: true,
+        message: 'Sending Request',
+        type: 'warning',
+      })
     );
-  }, [cart]);
+    const sendRequest = async () => {
+      const res = await fetch(
+        'https://react-http-9648f-default-rtdb.firebaseio.com/cartItems.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+      //send state a request is successful
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sent request to Database Successfully',
+          type: 'success',
+        })
+      );
+    };
+    sendRequest().catch((err) => {
+      //send error state
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: 'Sending Request Fail',
+          type: 'error',
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
     <div className='App'>
-      <Notification type='Success' message={'This Message'} />
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Alert } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../store/ui-slice';
 
 const Notification = ({ type, message }) => {
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    dispatch(
+      uiActions.showNotification({
+        open: false,
+      })
+    );
+  };
+
   return (
     <div>
-      <Alert severity={type}> {message}</Alert>
+      {notification && (
+        <Alert onClose={handleClose} severity={type}>
+          {' '}
+          {message}
+        </Alert>
+      )}
     </div>
   );
 };
